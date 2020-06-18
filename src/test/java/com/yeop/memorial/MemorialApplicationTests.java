7a8767ba18112f444ddbaae7f570222e0c391356
seirian <seirian@gmail.com>
2020-06-18 16:07:27 +0900

    Initial commit

---
+++ src/test/java/com/yeop/memorial/MemorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeop.memorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
